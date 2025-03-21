c8a13b1b1d6c4c6a2ad8ab729d6bd497c7a4580b
IleanaBls <ileana.bolas.16@gmail.com>
2025-03-21 15:13:11 +0100

    add deleteOrganisation function

--- src/lib/organisationQuery.ts
+++ src/lib/organisationQuery.ts
@@ -4,6 +4,7 @@ import { Organisation, organisation } from "./schema/schema";
 import { db } from "./drizzle";
 import { revalidatePath } from "next/cache";
 import { replaceEmptyValueByNull } from "./utils";
+import { eq } from "drizzle-orm";
 
 export const getOrganisations = async () => {
   const selectResult = await db.select().from(organisation);
@@ -31,3 +32,11 @@ export const addOrganisation = async (formData: any) => {
     console.error("the organisation has not been added to the database");
   }
 };
+
+export const deleteOrganisation = async(organisationId: string) => {
+  const result = await db.delete(organisation).where(eq(organisation.id, organisationId)).returning({deleted: organisation.id})
+  if(result[0].deleted){
+    return {result: result}
+  }
+  return {error: "Organisation has not been deleted"}
+}
