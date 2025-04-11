872525b42cb2f72e4b4fd2f68e1b8d72241b6c88
synowski-jessica <synowski.jessica@hotmail.fr>
2025-04-11 18:03:59 +0200

    feat(contactForm):add nodemailer

--- src/app/components/form/FormContact.tsx
+++ src/app/components/form/FormContact.tsx
@@ -1,45 +1,43 @@
 "use client";
 
-import { FormEvent } from "react";
+import { sendMailFromContact } from "@/lib/utils";
 import CheckboxForm from "../CheckboxForm";
-import { InputFormRequired } from "../InputFormRequired";
+import { InputFormRequired } from "../inputs/InputFormRequired";
 import { TextAreaForm } from "../TextAreaForm";
+import nodemailer from "nodemailer";
+
 
 const FormContact = () => {
-  async function onSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+  const handleSubmit = async (formData: FormData) => {
 
-    const formData = new FormData(event.currentTarget);
-    const response = await fetch("/api/submit", {
-      method: "POST",
-      body: formData,
-    });
+    await sendMailFromContact(formData);
 
-    const data = await response.json();
-    console.log("Réponse du serveur :", data);
   }
 
+
   return (
-    <form onSubmit={onSubmit}>
-      <div className="mx-6 mt-10 px-4 py-10 shadow-md shadow-lightOliveGreen rounded-lg flex flex-col items-start justify-center gap-5">
-        <div>
-          <InputFormRequired type="text" name="Nom" label="Nom" placeholder="Nom" />
-        </div>
-        <div>
-          <InputFormRequired type="email" name="Adresse mail" label="Adresse mail" placeholder="Adresse mail" />
-        </div>
-        <div>
-          <TextAreaForm name="Message" label="Message" placeholder="Votre message ici..." />
-        </div>
-        <div>
-          <CheckboxForm
-            name="checkbox"
-            value="En soumettant ce formulaire, j'accepte que les informations saisies soient utilisées dans le but de me recontacter."
-          />
-        </div>
-        <button type="submit">Valider</button>
+    <>
+      <div className="mx-6 mt-10 px-4 py-10 shadow-md shadow-lightOliveGreen rounded-lg flex flex-col items-start justify-center gap-5 w-1/4 mx-auto">
+        <form action={handleSubmit}>
+          <div>
+            <InputFormRequired type="text" name="Nom" label="Nom" placeholder="Nom" />
+          </div>
+          <div>
+            <InputFormRequired type="email" name="Adresse mail" label="Adresse mail" placeholder="Adresse mail" />
+          </div>
+          <div>
+            <TextAreaForm name="Message" label="Message" placeholder="Votre message ici..." />
+          </div>
+          <div>
+            <CheckboxForm
+              name="checkbox"
+              value="En soumettant ce formulaire, j'accepte que les informations saisies soient utilisées dans le but de me recontacter."
+            />
+          </div>
+          <button type="submit">Valider</button>
+        </form>
       </div>
-    </form>
+    </>
   );
 };
 
