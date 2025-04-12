747d1b30b4f31aaadf4b2930fcaf2f26c54d7a08
synowski-jessica <synowski.jessica@hotmail.fr>
2025-04-12 22:52:57 +0200

    feat(contactForm):style to formContact

--- src/app/components/button/orangeButton.tsx
+++ src/app/components/button/orangeButton.tsx
@@ -1,18 +1,16 @@
-const OrangeButton = ({
-  label,
-  onClick,
-}: {
-  label: string;
-
-  onClick?: () => void;
-}) => {
+const OrangeButton = ({ label, onClick, type = "button", disabled = false }: { label: string; type?: "button" | "submit", onClick?: () => void, disabled?: boolean }) => {
   return (
     <>
       <button
+        type={type}
         onClick={onClick}
-        className="bg-lightOrange  text-midnightBlue rounded-full p-2 font-title uppercase mb-4"
+        disabled={disabled}
+        className={`${disabled
+          ? "bg-gray-400 text-gray-600 cursor-not-allowed"
+          : "c-btn--orange text-midnightBlue"
+          } c-btn c-btn--r50 px-16 py-4 font-title uppercase mb-4 text-lg transition-all duration-200 lg:px-32`}
       >
-        {label}
+        <span>{label}</span>
       </button>
     </>
   );
