e0f7c4941c22508be629706a80542a701829c03d
synowski-jessica <synowski.jessica@hotmail.fr>
2025-04-18 14:28:14 +0200

    feat(contactPage):add background

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -1,5 +1,6 @@
 import ContactForm from "@/app/components/form/contactForm";
 import { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Contact - Access Collect",
@@ -9,11 +10,21 @@ export const metadata: Metadata = {
 const ContactPage = () => {
   return (
     <>
-      <div className="bg-[url('/backgroundContact.jpg')]">
-        <h1 className="text-brightOrange text-center text-xl mt-8 font-title font-bold tracking-widest">
-          FORMULAIRE DE CONTACT
-        </h1>
-        <ContactForm />
+      <div className="relative min-h-screen " >
+        <div className="absolute inset-0 ">
+          <Image
+              src="/backgroundContact.jpg"
+              alt="background"
+              layout="fill">
+          </Image>
+
+          <div className="relative">
+            <h1 className="text-brightOrange text-center text-xl mt-8 font-title font-bold tracking-widest">
+              {"FORMULAIRE DE CONTACT"}
+            </h1>
+            <ContactForm />
+        </div>
+        </div>
       </div>
     </>
   );
