800c3957f639a626c8aaece7fecf18cec99ebd92
Christopher <nkchrs@hotmail.fr>
2025-05-02 12:01:34 +0200

    Correction of pr

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,5 +1,20 @@
 import { redirect } from "next/navigation";
 
 export function redirectToHomepage() {
-  redirect("/dashboard");
+  redirect("/");
+}
+
+
+export function redirectToLogin() {
+  redirect("/login")
+}
+
+
+export function redirectToDashboard() {
+  redirect("/dashboard")
+}
+
+
+export function redirectToForgottenPassword() {
+  redirect("/forgotten-password")
 }
